c8fa929ce114af6e2da6f00c48e7f1cb2fb13fc9
Juhi Rajani <juhirajani@Juhis-Laptop.local>
2024-09-12 08:48:57 +0530

    assignment-2 changes

--- Assignment-2/2.c
+++ Assignment-2/2.c
@@ -7,26 +7,22 @@ int main () {
 
     struct rlimit no_file, rl_cpu, rl_stack, rl_fsize;
 
-	if (getrlimit(RLIMIT_NOFILE, &no_file) == 0)
-	{
+	if (getrlimit(RLIMIT_NOFILE, &no_file) == 0) {
               printf("RLIMIT_NOFILE limits - soft limit= %llu \t"
 			" hard limit= %llu \n", no_file.rlim_cur,no_file.rlim_max);
 	}  
 
-    if (getrlimit(RLIMIT_CPU, &rl_cpu) == 0)
-	{
+    if (getrlimit(RLIMIT_CPU, &rl_cpu) == 0) {
               printf("RLIMIT_CPU limits - soft limit= %llu \t"
 			" hard limit= %llu \n", rl_cpu.rlim_cur,rl_cpu.rlim_max);
 	}
 
-    if (getrlimit(RLIMIT_STACK, &rl_stack) == 0)
-	{
+    if (getrlimit(RLIMIT_STACK, &rl_stack) == 0) {
               printf("RLIMIT_STACK limits - soft limit= %llu \t"
 			" hard limit= %llu \n", rl_stack.rlim_cur,rl_stack.rlim_max);
 	}
 
-    if (getrlimit(RLIMIT_FSIZE, &rl_fsize) == 0)
-	{
+    if (getrlimit(RLIMIT_FSIZE, &rl_fsize) == 0) {
               printf("RLIMIT_FSIZE limits - soft limit= %llu \t"
 			" hard limit= %llu \n", rl_fsize.rlim_cur,rl_fsize.rlim_max);
 	}
