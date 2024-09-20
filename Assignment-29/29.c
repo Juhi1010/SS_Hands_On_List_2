4c22dec84600d3d8de5f7a6809f0a95010ab7420
Juhi Rajani <juhirajani@Juhis-Laptop.local>
2024-09-20 22:15:03 +0530

    assignment 29

---
+++ Assignment-29/29.c
@@ -0,0 +1,32 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include <sys/types.h>
+#include <sys/ipc.h>
+#include <sys/msg.h>
+
+int main() {
+
+    key_t key;
+    int msgId;
+
+    key = ftok("/tmp", 'a');
+    if (key == -1) {
+        perror("ftok");
+        exit(EXIT_FAILURE);
+    }
+
+    msgId = msgget(key, 0666);
+    if (msgId == -1) {
+        perror("msgget");
+        exit(EXIT_FAILURE);
+    }
+
+    if (msgctl(msgId, IPC_RMID, NULL) == -1) {
+        perror("msgctl IPC_RMID");
+        exit(EXIT_FAILURE);
+    }
+
+    printf("Message queue with id %d removed successfully.\n", msgId);
+
+    return 0;
+}
