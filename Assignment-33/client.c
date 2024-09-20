432055546d7bc30a0707115e85be19488b663a29
Juhi Rajani <juhirajani@Juhis-Laptop.local>
2024-09-20 22:59:53 +0530

    assignment 33

---
+++ Assignment-33/client.c
@@ -0,0 +1,32 @@
+============================================================================
+Name : client.c
+Author : Juhi Rajani
+Description :  33. Write a program to communicate between two machines using socket.
+Date: 18th Sept, 2024.
+============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main() {
+    int sock;
+    struct sockaddr_in serv_addr;
+    char *msg = "Hello i am client";
+    char buffer[512];
+
+    sock = socket(AF_INET, SOCK_STREAM, 0);
+    serv_addr.sin_family = AF_INET;
+    serv_addr.sin_port = htons(8080);
+    inet_pton(AF_INET, "127.0.0.1", &serv_addr.sin_addr);
+
+    connect(sock, (struct sockaddr *)&serv_addr, sizeof(serv_addr));
+    send(sock, msg, strlen(msg), 0);
+    read(sock, buffer, 512);
+
+    printf("Message from server - %s\n", buffer);
+    close(sock);
+    return 0;
+}
---
+++ Assignment-33/server.c
@@ -0,0 +1,37 @@
+============================================================================
+Name : server.c
+Author : Juhi Rajani
+Description :  33. Write a program to communicate between two machines using socket.
+Date: 18th Sept, 2024.
+============================================================================
+
+#include <stdio.h>
+#include <stdlib.h>
+#include <string.h>
+#include <unistd.h>
+#include <arpa/inet.h>
+
+int main() {
+    int server, new_sock;
+    struct sockaddr_in address;
+    char buffer[512];
+
+    server = socket(AF_INET, SOCK_STREAM, 0);
+    address.sin_family = AF_INET;
+    address.sin_addr.s_addr = INADDR_ANY;
+    address.sin_port = htons(8080);
+
+    bind(server, (struct sockaddr *)&address, sizeof(address));
+    listen(server, 3);
+    
+    new_sock = accept(server, NULL, NULL);
+    read(new_sock, buffer, 512);
+
+    printf("Message received from client - %s\n", buffer);
+
+    send(new_sock, buffer, strlen(buffer), 0);
+
+    close(new_sock);
+    close(server);
+    return 0;
+}
