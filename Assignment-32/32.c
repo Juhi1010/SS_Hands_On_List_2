3ca07611a7b350c0be5f45a83ce7dbeb1c51236a
Juhi Rajani <juhirajani@Juhis-Laptop.local>
2024-09-20 23:53:38 +0530

    32

--- Assignment-32/32.c
+++ Assignment-32/32.c
@@ -1,3 +1,14 @@
+==================================================================================
+Name : 32.c
+Author : Juhi Rajani
+Description :  32. Write a program to implement semaphore to protect any critical section.
+                a. rewrite the ticket number creation program using semaphore
+                b. protect shared memory from concurrent write access
+                c. protect multiple pseudo resources ( may be two) using counting semaphore
+                d. remove the created semaphore 
+Date: 20th Sept, 2024.
+===================================================================================
+
 #include <stdio.h>
 #include <pthread.h>
 #include <semaphore.h>
